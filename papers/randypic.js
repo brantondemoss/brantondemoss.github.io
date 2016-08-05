e95430b1b971a2f9eec3c76d244008d94e76dc79
Branton Demoss <branton.demoss@colorado.edu>
2016-08-05 00:45:39 -0600

    added pic

--- papers/randypic.js
+++ papers/randypic.js
@@ -1,5 +1,5 @@
 window.onload = choosePic;
-var myPix = new Array("../resources/eli1.png","../resources/eli2.png","../resources/eli3.png","../resources/eli4.png","../resources/eli5.png");
+var myPix = new Array("../resources/eli1.png","../resources/eli2.png","../resources/eli3.png","../resources/eli4.png","../resources/eli5.png","../resources/orientedreidmoves.png");
 
 function choosePic() {
     var randomNum = Math.floor(Math.random() * myPix.length);
