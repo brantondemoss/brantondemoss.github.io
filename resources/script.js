5f134034e3c85c282b8c7014432728985bffd889
Branton <brantondemoss@gmail.com>
2020-06-23 17:59:45 -0600

    updated script

--- resources/script.js
+++ resources/script.js
@@ -6,19 +6,30 @@ function pixTimeChange() {
     var r3="resources/CirclePluto.png";
     var r4="resources/psycplutoround.jpg"
     var el=document.getElementById('myimage');
+    var main=document.getElementById('mainImage');
     
     if(h>=7 && h<21)
-    {el.src =  r1;}
+    {
+	var rand = Math.floor(Math.random() * 2)
+
+	if (rand == 0) { main.innerHTML += "<a href='/sunflower'><img class='rounded' src='resources/blacksunflowervector.svg' id='myimage' title='this image changes based on time of day'></a>"}
+	else{ main.innerHTML += '<a href="https://brantondemoss.com/seismicdream"><video loop autoplay muted playsinline width="420" style="float: left; padding-left: 25%;padding-top: 5px;"><source src="seismicdream/4980.mp4" type="video/mp4"></video>'}
+	
+	
+    }
     else if(h==4)
-    {el.src = r4;
+    {
+	main.innerHTML += "<a href='/sunflower'><img class='rounded' src='resources/psycplutoround.jpg' id='myimage' title='this image changes based on time of day'></a>"
         document.getElementById("plutotext").innerHTML += "<p class='med just'>It's 4 in the morning, what are you doing here!?</p>";
     }
     else if(h<6 && h>=1)
-    {el.src = r3;
+    {
+	main.innerHTML += "<a href='/sunflower'><img class='rounded' src='resources/CirclePluto.png' id='myimage' title='this image changes based on time of day'></a>"
         document.getElementById("plutotext").innerHTML +="<p class='med just'>It's a lonely time of night. Enjoy Pluto :-)</p>";
     }
     else
-    {el.src = r2;
+    {
+	main.innerHTML += "<a href='/sunflower'><img class='rounded' src='resources/CircleMoon.png' id='myimage' title='this image changes based on time of day'></a>"
         /*document.getElementById("plutotext").innerHTML +="<p class='med just'>it's only a liquid crystal moon</p>";*/
     }
 }
